50f81feebe6f63e443f85486b161862a2464926e
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-23 11:45:32 +0200

    Add project files.

---
+++ t03_05_Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace t03_05_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95F;
+            char character = 'A';
+            string firstName = "Bomba";
+            bool isWorking = true;
+            var isVariable = true;
+            const float Pi=3.14F;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(isVariable);
+            Console.WriteLine(Pi);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
---
+++ t03_07_TypeConversion/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace t03_07_TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            int iCast = 2;
+            byte bCast = (byte)iCast;
+            Console.WriteLine(bCast);
+
+            string number = "1234";
+            int num = Convert.ToInt32(number);
+            Console.WriteLine(num);
+
+            try
+            {
+                byte bConvert = Convert.ToByte(number);
+                Console.WriteLine(bConvert);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to byte.");
+            }
+
+            try
+            {
+                string str = "true";
+                bool bBool = Convert.ToBoolean(str);
+                Console.WriteLine(bBool);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
