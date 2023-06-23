271eab1bdad163fe662219540cd4f53a22ad3d43
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-23 12:59:09 +0200

    Operators

--- t03_10_Operators/Program.cs
+++ t03_10_Operators/Program.cs
@@ -7,6 +7,23 @@
             var a = 10;
             var b = 3;
             Console.WriteLine(a + b);
+            Console.WriteLine((float)a / (float)b);
+
+            a = 1;
+            b = 2;
+            var c = 3;
+            Console.WriteLine(a + b * c);
+            Console.WriteLine((a + b) * c);
+
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b));
+
+            Console.WriteLine(c > b && c > a);
+            Console.WriteLine(c > b && c == a);
+            Console.WriteLine(c > b || c == a);
+            Console.WriteLine(!(c > b) || c == a);
         }
     }
 }
