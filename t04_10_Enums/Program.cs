24916566a31e06dc71cd91c959267c90ebe7ef56
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-24 12:40:01 +0200

    Enums

---
+++ t04_10_Enums/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace t04_10_Enums
+{
+    internal enum ShippingMethodNoValueSet
+    {
+        RegularAirMail,
+        RegisteredAirMail,
+        Express
+    }
+
+    internal enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.RegularAirMail;
+            Console.WriteLine((int)method);
+
+            var methodId = 2;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+            Console.WriteLine(method);
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}
