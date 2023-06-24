f80c1d430c65783d591008d1f337e5f75a966e5e
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-24 11:39:12 +0200

    Classes

---
+++ t04_03_Classes/Pearson.cs
@@ -0,0 +1,13 @@
+﻿namespace t04_03_Classes
+{
+    public class Pearson
+    {
+        public string? FirstName;
+        public string? LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine($"My name is {LastName}, {FirstName} {LastName}.");
+        }
+    }
+}
---
+++ t04_03_Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using t04_03_Classes.Math;
+
+namespace t04_03_Classes
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pearson james = new()
+            {
+                FirstName = "James",
+                LastName = "Bond"
+            };
+            james.Introduce();
+
+            Calculator calculator = new();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
