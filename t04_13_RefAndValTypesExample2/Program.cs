d13c16e079a59689703cfeab6d0b11a4027e6a3d
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-24 13:46:53 +0200

    RefAndValTypesExample2

---
+++ t04_13_RefAndValTypesExample2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace t04_13_RefAndValTypesExample2
+{
+    internal class Pearson
+    {
+        internal int Age;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var pearson = new Pearson() { Age = 20 };
+            MakeOld(pearson);
+            Console.WriteLine(pearson.Age);
+        }
+
+        internal static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        internal static void MakeOld(Pearson pearson)
+        {
+            pearson.Age += 10;
+        }
+    }
+}
