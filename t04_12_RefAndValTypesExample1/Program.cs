2acbba9da367fecba07702a9408f61761225c75d
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-24 13:36:54 +0200

    RefAndValTypesExample1

---
+++ t04_12_RefAndValTypesExample1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace t04_12_RefAndValTypesExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1[0]: {0}, array1[0]: {1}", array1[0], array2[0]));
+        }
+    }
+}
