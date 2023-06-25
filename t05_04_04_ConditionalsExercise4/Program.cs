e08d5a3ac964a6ed108c16a9879bf7579eeb2be4
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-25 13:15:41 +0200

    ConditionalsExercise3

--- t05_04_04_ConditionalsExercise4/Program.cs
+++ t05_04_04_ConditionalsExercise4/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Your job is to write a program for a speed camera.
+            // For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.
+            // Write a program that asks the user to enter the speed limit.
+            // Once set, the program asks for the speed of a car.
+            // If the user enters a value less than the speed limit, program should display Ok on the console.
+            // If the value is above the speed limit, the program should calculate the number of demerit points.
+            // For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console.
+            // If the number of demerit points is above 12, the program should display License Suspended.
+
+            Console.WriteLine("Please enter a speed limit:");
+            if (!int.TryParse(Console.ReadLine(), out int speedLimit))
+            {
+                WrongInput();
+            }
+
+            Console.WriteLine("Please enter a car speed:");
+            if (!int.TryParse(Console.ReadLine(), out int carSpeed))
+            {
+                WrongInput();
+            }
+        }
+
+        internal static void WrongInput()
+        {
+            Console.WriteLine($"Wrong input. Please enter number between greater then 0.");
+            System.Environment.Exit(13);
         }
     }
 }
