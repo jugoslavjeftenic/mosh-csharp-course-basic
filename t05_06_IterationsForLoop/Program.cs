241d482ae69f512bf0a96925e010af25bdfecee8
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-25 14:07:56 +0200

    IterationsForLoop

--- t05_06_IterationsForLoop/Program.cs
+++ t05_06_IterationsForLoop/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace t05_06_Iterations
+﻿namespace t05_06_IterationsForLoop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int i = 10; i > 0; i--)
             {
                 if (i % 2 == 0)
                 {
