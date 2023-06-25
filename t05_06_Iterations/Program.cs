7d3a15ab0445dc49b31cea4ba8db309ab9b4b7e6
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-25 13:59:46 +0200

    ConditionalsExercise4

---
+++ t05_06_Iterations/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace t05_06_Iterations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
