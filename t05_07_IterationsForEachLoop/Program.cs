3b14702d2c76a8eeadc9cb5f30068625ef8893ad
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-25 14:24:42 +0200

    IterationsForEachLoop

---
+++ t05_07_IterationsForEachLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace t05_07_IterationsForEachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "James Bond";
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            Console.WriteLine("-----");
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            Console.WriteLine("-----");
+
+            var numbers = new int[] { 1, 2, 3, 4 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
