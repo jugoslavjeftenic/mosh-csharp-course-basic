87154cf6a0e4665b76dc0b1c438825adb7b4a275
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-25 11:49:22 +0200

    ConditionalsExercise2

---
+++ t05_04_02_ConditionalsExercise2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace t05_04_02_ConditionalsExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program which takes two numbers from the console and displays the maximum of the two.
+
+            Console.WriteLine("Please enter first number:");
+            if(!int.TryParse(Console.ReadLine(),out int num1))
+            {
+                WrongInput();
+            }
+
+            Console.WriteLine("Please enter second number:");
+            if(!int.TryParse(Console.ReadLine(),out int num2))
+            {
+                WrongInput();
+            }
+
+            if(num1 < int.MinValue || num1 > int.MaxValue)
+            {
+                WrongInput();
+            }
+
+            if(num2 < int.MinValue || num1 > int.MaxValue)
+            {
+                WrongInput();
+            }
+
+            if(num1 > num2)
+            {
+                Console.WriteLine("First number is greater then second number.");
+            }
+            else if(num1 < num2)
+            {
+                Console.WriteLine("Second number is greater then first number.");
+            }
+            else
+            {
+                Console.WriteLine("Numbers are equal.");
+            }
+        }
+
+        internal static void WrongInput()
+        {
+            Console.WriteLine($"Wrong input. Please enter number between {int.MinValue} and {int.MaxValue}.");
+            System.Environment.Exit(13);
+        }
+    }
+}
