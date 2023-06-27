b61ff7ae60fd540e41c735dbcde5c21c9bc859eb
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-27 19:24:53 +0200

    t05_12_04_ControlFlowExercise4

---
+++ t05_12_04_ControlFlowExercise4/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace t05_12_04_ControlFlowExercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that picks a random number between 1 and 10.
+            // Give the user 4 chances to guess the number.
+            // If the user guesses the number, display "You won"; otherwise, display "You lost".
+            // (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+
+            int random = new Random().Next(1, 100);
+            int attempts = 4;
+
+            while (attempts > 0)
+            {
+                Console.Write("Guess a number or \"e\" to exit: ");
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out int number))
+                {
+                    attempts--;
+                    if (number == random)
+                    {
+                        Console.WriteLine("You won");
+                        System.Environment.Exit(0);
+                    }
+                    Console.WriteLine($"Try guessing again (you have {attempts} attempts left).");
+                }
+                else if (input?.Trim().ToLower() == "e")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input.");
+                }
+            }
+            Console.WriteLine("You lost");
+            Console.WriteLine($"Random number was {random}.");
+        }
+    }
+}
---
+++ t05_12_05_ControlFlowExercise5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace t05_12_05_ControlFlowExercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program and ask the user to enter a series of numbers separated by comma.
+            // Find the maximum of the numbers and display it on the console.
+            // For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
