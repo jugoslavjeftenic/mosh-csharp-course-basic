148c9d26835b6a74bd3af88c15573fe4fc21a657
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-27 20:29:50 +0200

    ControlFlowExercise5

--- t05_12_05_ControlFlowExercise5/Program.cs
+++ t05_12_05_ControlFlowExercise5/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace t05_12_05_ControlFlowExercise5
+﻿using System;
+
+namespace t05_12_05_ControlFlowExercise5
 {
     internal class Program
     {
@@ -8,7 +10,41 @@
             // Find the maximum of the numbers and display it on the console.
             // For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a series of numbers separated by comma (example \"5, 3, 8, 1, 4\"):");
+            var input = Console.ReadLine();
+
+            char[] delimiterChars = { ' ', ',', '.', ':', ';', '\t' };
+            var inputArray = input?.Split(delimiterChars, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            int maximum = 0;
+
+            if (inputArray?.Length > 0)
+            {
+                int[] numbers = new int[inputArray.Length];
+
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    if (int.TryParse(inputArray[i], out int numb))
+                    {
+                        numbers[i] = numb;
+                    }
+                    else
+                    {
+                        numbers[i] = 0;
+                    }
+                }
+
+                maximum = numbers[0];
+                foreach (var number in numbers)
+                {
+                    if (maximum < number)
+                    {
+                        maximum = number;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Maximum of the numbers is: {maximum}.");
         }
     }
 }
