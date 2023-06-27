cfc0aeff1f492135fb2885b0e25e81e4f4cb73d8
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-27 11:01:36 +0200

    RandomClass

---
+++ t05_09_RandomClass/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace t05_09_RandomClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(random.Next());
+                Console.Write(":");
+                Console.Write(random.Next(10));
+                Console.Write(":");
+                Console.Write(random.Next(1, 10));
+                Console.Write(":");
+                Console.WriteLine((char)random.Next(97, 122));
+            }
+
+            const int passwordLength = 20;
+            var buffer = new char[passwordLength];
+            for (int i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
