41254bb058011962e4db278a47268a17fb9ba952
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-27 11:15:36 +0200

    ControlFlowExercise1

---
+++ t05_12_01_ControlFlowExercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace t05_12_01_ControlFlowExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+            // Display the count on the console.
+
+            int count = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
