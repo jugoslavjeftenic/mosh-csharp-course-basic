2d57e0bc5211cd98ff1eaab45b95a1502a40ed1c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-27 11:37:24 +0200

    ControlFlowExercise2

---
+++ t05_12_02_ControlFlowExercise2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace t05_12_02_ControlFlowExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program and continuously ask the user to enter a number or "ok" to exit.
+            // Calculate the sum of all the previously entered numbers and display it on the console.
+
+            int sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number or \"ok\" to exit: ");
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out int number))
+                {
+                    sum += number;
+                    Console.WriteLine("Sum of numbers: " + sum);
+                }
+                else if (input?.ToLower() == "ok")
+                {
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input. Enter a number or \"ok\" to exit: ");
+                }
+            }
+        }
+    }
+}
