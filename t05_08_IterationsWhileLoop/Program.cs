35b3e6344b0f777c3630854f8b610544765d41f4
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-27 10:34:13 +0200

    IterationsWhileLoop

---
+++ t05_08_IterationsWhileLoop/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace t05_08_IterationsWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int y = 0; y <= 10; y++)
+            {
+                if (y % 2 == 0)
+                {
+                    Console.WriteLine(y);
+                }
+            }
+
+            int i = 0;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                Console.WriteLine("@Echo: " + input);
+            }
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
