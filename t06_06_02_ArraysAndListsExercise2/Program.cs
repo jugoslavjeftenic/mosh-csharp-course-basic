6d81eb6c8e5c6afe89462679f4ee8c0c15f354c5
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-01 08:28:49 +0200

    ArraysAndListsExercise2

--- t06_06_02_ArraysAndListsExercise2/Program.cs
+++ t06_06_02_ArraysAndListsExercise2/Program.cs
@@ -8,7 +8,14 @@
             // Use an array to reverse the name and then store the result in a new string.
             // Display the reversed name on the console.
 
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter your name: ");
+            var input = Console.ReadLine()?.Trim();
+
+            if (input?.Length > 0)
+            {
+                string? reversedName = input.ToArray<char>().Reverse().ToString();
+                Console.WriteLine(reversedName);
+            }
         }
     }
 }
