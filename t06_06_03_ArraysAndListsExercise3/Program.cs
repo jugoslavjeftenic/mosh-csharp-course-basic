93007b90d2455428b8b995e537da2a510c7fbb9a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-01 17:05:03 +0200

    ArraysAndListsExercise3

--- t06_06_03_ArraysAndListsExercise3/Program.cs
+++ t06_06_03_ArraysAndListsExercise3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace t06_06_03_ArraysAndListsExercise3
+﻿using System;
+
+namespace t06_06_03_ArraysAndListsExercise3
 {
     internal class Program
     {
@@ -8,7 +10,37 @@
             // If a number has been previously entered, display an error message and ask the user to re-try.
             // Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
 
-            Console.WriteLine("Hello, World!");
+            int[] numbers = new int[5];
+            int attempt = 5;
+
+            while (attempt > 0)
+            {
+                Console.Write($"Enter a number [attempt {attempt}]: ");
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out int number))
+                {
+                    if (!numbers.Contains(number))
+                    {
+                        attempt--;
+                        numbers[attempt] = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number has been previously entered.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong number.");
+                }
+            }
+
+            Array.Sort(numbers);
+            foreach (var n in numbers)
+            {
+                Console.Write($"{n}, ");
+            }
         }
     }
 }
