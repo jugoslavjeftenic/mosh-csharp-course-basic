feac63f43fedc6138701fe38fad2976e973b4a88
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-04 17:47:58 +0200

    SummarisingText

---
+++ t08_04_SummarisingText/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace t08_04_SummarisingText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long text.";
+            Console.WriteLine(StringUtility.SummerizeText(sentence));
+        }
+    }
+}
