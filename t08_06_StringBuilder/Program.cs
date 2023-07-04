6fa7c91b9b345e8f0c143e4a5a8bf6f0ac3bfb54
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-04 18:05:51 +0200

    StringBuilder

--- t08_06_StringBuilder/Program.cs
+++ t08_06_StringBuilder/Program.cs
@@ -15,11 +15,10 @@ namespace t08_06_StringBuilder
                 .AppendLine()
                 .Append('-', 10);
 
-            builder.Replace('-', '+');
-
-            builder.Remove(0, 10);
-
-            builder.Insert(0, new string('-', 10));
+            builder
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
 
             Console.WriteLine(builder);
             Console.WriteLine(builder[12]);
