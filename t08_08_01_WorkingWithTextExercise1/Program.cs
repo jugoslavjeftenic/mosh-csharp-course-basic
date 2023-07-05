7b15147901be580bf3feac12c00d0dad337e7aeb
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-05 09:24:34 +0200

    WorkingWithTextExercise1

--- t08_08_01_WorkingWithTextExercise1/Program.cs
+++ t08_08_01_WorkingWithTextExercise1/Program.cs
@@ -9,7 +9,56 @@
             // For example, if the input is "5-6-7-8-9" or "20-19-18-17-16",
             // display a message: "Consecutive"; otherwise, display "Not Consecutive".
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter at least two numbers separated by a hyphen (example \"5-6-7-8-9\"):");
+            var input = Console.ReadLine();
+
+            char[] delimiterChars = { ' ', ',', '.', ':', ';', '-', '\t' };
+            string[]? inputArray = input?.Split(delimiterChars, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            if (inputArray == null || inputArray.Length < 2)
+            {
+                Console.WriteLine("Invalid List. Please re-try.");
+                System.Environment.Exit(13);
+            }
+
+            int[] numbers = Array.ConvertAll(inputArray, s => int.TryParse(s, out var x) ? x : -1);
+            if (numbers.Length < 2)
+            {
+                Console.WriteLine("Invalid List. Please re-try.");
+                System.Environment.Exit(13);
+            }
+
+            bool isAscConsecutive = false;
+            bool isDscConsecutive = false;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] + 1 != numbers[i + 1])
+                {
+                    isAscConsecutive = false;
+                    break;
+                }
+                isAscConsecutive = true;
+            }
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] - 1 != numbers[i + 1])
+                {
+                    isDscConsecutive = false;
+                    break;
+                }
+                isDscConsecutive = true;
+            }
+
+            if (isAscConsecutive || isDscConsecutive)
+            {
+                Console.WriteLine("Consecutive");
+            }
+            else
+            {
+                Console.WriteLine("Not Consecutive");
+            }
         }
     }
 }
