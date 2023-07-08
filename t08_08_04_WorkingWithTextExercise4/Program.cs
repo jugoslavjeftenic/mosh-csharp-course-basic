5cca2d81b24896732e37ccae3b32ccb92b926377
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-08 10:40:44 +0200

    WorkingWithTextExercise4

--- t08_08_04_WorkingWithTextExercise4/Program.cs
+++ t08_08_04_WorkingWithTextExercise4/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace t08_08_04_WorkingWithTextExercise4
+﻿using System.Text;
+
+namespace t08_08_04_WorkingWithTextExercise4
 {
     internal class Program
     {
@@ -10,7 +12,28 @@
             // Make sure that the program is not dependent on the input.
             // So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a few words separated by a space:");
+            var input = Console.ReadLine();
+            if (input == null || input.Length < 1)
+            {
+                Console.WriteLine("Invalid input. Please re-try.");
+                System.Environment.Exit(13);
+            }
+
+            var words = input.Split(' ');
+            if (words != null || words?.Length > 0)
+            {
+                var stringBuilder = new StringBuilder();
+                foreach (var word in words)
+                {
+                    stringBuilder.Append(char.ToUpper(word[0]));
+                    if (word.Length > 1)
+                    {
+                        stringBuilder.Append(word[1..].ToLower());
+                    }
+                }
+                Console.WriteLine(stringBuilder);
+            }
         }
     }
 }
