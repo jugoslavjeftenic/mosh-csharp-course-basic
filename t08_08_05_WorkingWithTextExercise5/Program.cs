63a06ba59bf2ee15fd19e30242cb268bf3e0c467
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-08 10:56:37 +0200

    WorkingWithTextExercise5

--- t08_08_05_WorkingWithTextExercise5/Program.cs
+++ t08_08_05_WorkingWithTextExercise5/Program.cs
@@ -8,7 +8,16 @@
             // Count the number of vowels (a, e, o, u, i) in the word.
             // So, if the user enters "inadequate", the program should display 6 on the console.
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter an English word:");
+            var input = Console.ReadLine();
+            if (input == null || input.Length < 1)
+            {
+                Console.WriteLine("Invalid input. Please re-try.");
+                System.Environment.Exit(13);
+            }
+
+            int total = input.Count(c => "aeiou".Contains(Char.ToLower(c)));
+            Console.WriteLine($"Total vowel count: {total}");
         }
     }
 }
