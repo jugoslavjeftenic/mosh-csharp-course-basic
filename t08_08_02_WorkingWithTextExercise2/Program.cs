e83f5860ab4d9a4c57ddbdd97f34dd3371b5e2f0
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-08 09:53:10 +0200

    WorkingWithTextExercise2

--- t08_08_02_WorkingWithTextExercise2/Program.cs
+++ t08_08_02_WorkingWithTextExercise2/Program.cs
@@ -8,7 +8,30 @@
             // If the user simply presses Enter, without supplying an input, exit immediately;
             // otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
 
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Enter a few numbers separated by a hyphen (example \"5-1-7-8-5\"):");
+            var input = Console.ReadLine();
+
+            char[] delimiterChars = { ' ', ',', '.', ':', ';', '-', '\t' };
+            string[]? inputArray = input?.Split(delimiterChars, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            if (inputArray == null || inputArray.Length < 1)
+            {
+                Console.WriteLine("Invalid List. Please re-try.");
+                System.Environment.Exit(13);
+            }
+
+            int[] numbersArray = Array.ConvertAll(inputArray, s => int.TryParse(s, out var x) ? x : -1);
+            var duplicates = numbersArray.GroupBy(x => x)
+              .Where(g => g.Count() > 1)
+              .Select(y => y.Key)
+              .ToList();
+
+            if (duplicates.Count > 0)
+            {
+                Console.WriteLine("Duplicate");
+                Console.WriteLine(String.Join(", ", duplicates));
+            }
         }
     }
 }
