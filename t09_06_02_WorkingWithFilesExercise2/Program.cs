3fe0d443099d14f92a128f884ecd393fa3c44f09
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-10 20:22:10 +0200

    WorkingWithFilesExercise2

--- t09_06_02_WorkingWithFilesExercise2/Program.cs
+++ t09_06_02_WorkingWithFilesExercise2/Program.cs
@@ -6,7 +6,26 @@
         {
             // Write a program that reads a text file and displays the longest word in the file.
 
-            Console.WriteLine("Hello, World!");
+            string textFile = @"E:\SDK-github\course-mosh\c# 1 - basic\t09_06_02_WorkingWithFilesExercise2\example.txt";
+
+            if (File.Exists(textFile))
+            {
+                string text = File.ReadAllText(textFile);
+
+                char[] delimiterChars = { ' ', ',', '.', ':', ';', '-', '\t' };
+                string[] words = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+
+                string longestWord = "";
+                foreach (var word in words)
+                {
+                    if (word.Length > longestWord.Length)
+                    {
+                        longestWord = word;
+                    }
+                }
+
+                Console.WriteLine($"Longest word is \"{longestWord}\".");
+            }
         }
     }
 }
