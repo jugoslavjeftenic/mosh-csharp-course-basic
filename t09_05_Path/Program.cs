7ed579c984135a03a8102bbd11101774d71c0f7c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-10 13:59:15 +0200

    Path

---
+++ t09_05_Path/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace t09_05_Path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"E:\SDK-github\course-mosh\c# 1 - basic\t09_05_Path\Program.cs";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+            Console.WriteLine(extension);
+
+            Console.WriteLine($"Extension: {Path.GetExtension(path)}");
+            Console.WriteLine($"File name without of wxtension: {Path.GetFileNameWithoutExtension(path)}");
+            Console.WriteLine($"File name: {Path.GetFileName(path)}");
+            Console.WriteLine($"Directory name: {Path.GetDirectoryName(path)}");
+        }
+    }
+}
