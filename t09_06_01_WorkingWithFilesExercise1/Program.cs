9d4612aa3facfed3aa5480ca96505ea66e509fec
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-10 14:02:21 +0200

    WorkingWithFilesExercise1

---
+++ t09_06_01_WorkingWithFilesExercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace t09_06_01_WorkingWithFilesExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a text file and displays the number of words.
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ t09_06_02_WorkingWithFilesExercise2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace t09_06_02_WorkingWithFilesExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a text file and displays the longest word in the file.
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
