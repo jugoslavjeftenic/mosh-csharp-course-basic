520e8cd233b283f203bd4eeffedb3b09d4bdec7f
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-11 13:33:44 +0200

    DebuggingApplications

---
+++ t09_DebuggingApplications/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace t09_DebuggingApplications
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // F5        - run debugg mode
+            // Shift F5  - exit debugg mode
+            // F9        - break point
+            // F10       - step over
+            // F11       - step into
+            // Shift F11 - step out
+
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+    }
+}
