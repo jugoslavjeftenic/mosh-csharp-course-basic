7e7469390bcf4fd9fa874a356c691fe645191d74
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-12 12:34:27 +0200

    DebuggingApplications

--- t09_DebuggingApplications/Program.cs
+++ t09_DebuggingApplications/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace t09_DebuggingApplications
+﻿using System.Data;
+
+namespace t09_DebuggingApplications
 {
     internal class Program
     {
@@ -20,13 +22,24 @@
 
         public static List<int> GetSmallests(List<int> list, int count)
         {
+            if (list == null)
+            {
+                throw new ArgumentNullException("list");
+            }
+
+            if (count > list.Count || count < 1)
+            {
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in the list.");
+            }
+
+            var buffer = new List<int>(list);
             var smallests = new List<int>();
 
             while (smallests.Count < count)
             {
-                var min = GetSmallest(list);
+                var min = GetSmallest(buffer);
                 smallests.Add(min);
-                list.Remove(min);
+                buffer.Remove(min);
             }
 
             return smallests;
